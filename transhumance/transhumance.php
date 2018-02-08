1f88aaf742deb8628d3e6977786d7f0c011a0475
Unknown <lebihannathan@gmail.com>
2018-02-08 15:33:47 +0100

    premier essai

---
+++ transhumance/transhumance.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title>Déclaration de transhumance</title>
+
+    </head>
+    <body>
+        <h1 align="center"><b>Déclarer un transhumance intrarégionale</b></h1>
+        <h2>Renseignements responsable alpage</h2>
+        <form method="post" action="exo19_p3" name='form' onsubmit='return valider()'>
+            <table>
+                
+                <tr>
+                    <td><label>(*)Nom</label> :</td>
+                    <td><input type='text' name='nom_responsable' value =''></td>
+                </tr>
+                <tr>
+                    <td><label>(*)Prénom</label> :</td>
+                    <td><input type='text' name='prenom_responsable' value =''></td>
+                </tr>
+                <tr>
+                    <td> <label>(*)Numéro de téléphone:</label></td>
+                    <td><input type='text' name='num_responsable' value =''></td>
+                </tr>
+            </table>
+            <h2>Renseignements généraux</h2>
+            
+            
+        </form>
+    </body>
+
+
+
+
