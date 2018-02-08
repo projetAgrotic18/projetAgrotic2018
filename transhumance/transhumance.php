c78dde7980502da35a507edac10ebb2af72a4f74
Unknown <lebihannathan@gmail.com>
2018-02-08 15:56:44 +0100

    petiteavancée

--- transhumance/transhumance.php
+++ transhumance/transhumance.php
@@ -8,7 +8,7 @@
         <h2>Renseignements responsable alpage</h2>
         <form method="post" action="exo19_p3" name='form' onsubmit='return valider()'>
             <table>
-                
+
                 <tr>
                     <td><label>(*)Nom</label> :</td>
                     <td><input type='text' name='nom_responsable' value =''></td>
@@ -18,7 +18,7 @@
                     <td><input type='text' name='prenom_responsable' value =''></td>
                 </tr>
                 <tr>
-                    <td> <label>(*)Numéro de téléphone:</label></td>
+                    <td> <label>(*)Numéro de téléphone :</label></td>
                     <td><input type='text' name='num_responsable' value =''></td>
                 </tr>
             </table>
@@ -26,9 +26,55 @@
             (*)Date départ : 
             <input id="date_debut" type="date">
             (*)Date fin :
-            <input id="date_fin" type="date">
+            <input id="date_fin" type="date"><br><br>
+            <label>(*)Commune de destination</label>
+            <input type='text' name='num_responsable' value =''>
+            <h2>Vos animaux déplacés</h2>
+            <table>
+                <tr>
+                    <td>
+                        
+                    </td>
+                    <td>
+                        Agés de moins de 6 mois
+                    </td>
+                    <td>
+                        Agés de plus de 6 mois
+                    </td>
+                    
+                </tr>
+                <tr>
+                    <td>
+                        Caprins
+                    </td>
+                    <td>
+                        <input type='text' name='nbr_cap_-' value =''>
+                    </td>
+                    <td>
+                        <input type='text' name='nbr_cap_+' value =''>
+                    </td>
+                    
+                </tr>
+                <tr>
+                    <td>
+                        Ovins
+                    </td>
+                    <td>
+                        <input type='text' name='nbr_ov_-' value =''>
+                    </td>
+                    <td>
+                        <input type='text' name='nbr_ov_+' value =''>
+                    </td>
+                    
+                    
+                </tr>
+            </table>
+            Description du marquage :<br>
+            <TEXTAREA name="marquage" rows=10 cols=40></TEXTAREA>
             
-            
+            <input type="radio" name="type_paturage" value="collectif" /> Alpage/Pâturage collectif
+            <input type="radio" name="type_paturage" value="individuel" /> Alpage/Pâturage individuel
+
         </form>
     </body>
 
