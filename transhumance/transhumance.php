0ff9a3ce867edbf29676b9b1f25290c4d075eb34
Unknown <lebihannathan@gmail.com>
2018-02-08 16:08:13 +0100

    ptitcommit

--- transhumance/transhumance.php
+++ transhumance/transhumance.php
@@ -4,9 +4,9 @@
 
     </head>
     <body>
-        <h1 align="center"><b>Déclarer un transhumance intrarégionale</b></h1>
+        <h1 align="center"><b>Déclarer une transhumance intrarégionale</b></h1>
         <h2>Renseignements responsable alpage</h2>
-        <form method="post" action="exo19_p3" name='form' onsubmit='return valider()'>
+        <form method="post" action="validation_transhumance.php" name='form' >
             <table>
 
                 <tr>
@@ -27,13 +27,13 @@
             <input id="date_debut" type="date">
             (*)Date fin :
             <input id="date_fin" type="date"><br><br>
-            <label>(*)Commune de destination</label>
+            <label>(*)Commune de destination :</label>
             <input type='text' name='num_responsable' value =''>
             <h2>Vos animaux déplacés</h2>
             <table>
                 <tr>
                     <td>
-                        
+
                     </td>
                     <td>
                         Agés de moins de 6 mois
@@ -41,7 +41,7 @@
                     <td>
                         Agés de plus de 6 mois
                     </td>
-                    
+
                 </tr>
                 <tr>
                     <td>
@@ -53,7 +53,7 @@
                     <td>
                         <input type='text' name='nbr_cap_+' value =''>
                     </td>
-                    
+
                 </tr>
                 <tr>
                     <td>
@@ -65,15 +65,50 @@
                     <td>
                         <input type='text' name='nbr_ov_+' value =''>
                     </td>
-                    
-                    
+
+
                 </tr>
             </table>
             Description du marquage :<br>
-            <TEXTAREA name="marquage" rows=10 cols=40></TEXTAREA>
+            <TEXTAREA name="marquage" rows=10 cols=40></TEXTAREA><Br><br>
             
             <input type="radio" name="type_paturage" value="collectif" /> Alpage/Pâturage collectif
             <input type="radio" name="type_paturage" value="individuel" /> Alpage/Pâturage individuel
+            <h2>Transporteur</h2>
+            <table>
+                <tr>
+                    <td>
+                        <label>Nom :</label>
+                    </td>
+                    <td>
+                        <input type='text' name='nom_transp' value =''>
+                    </td>
+                    <td>
+                        <label>Adresse</label>
+                    </td>
+                    <td>
+                        <input type='text' name='adresse_transp' value =''>
+                    </td>
+                    
+                </tr>
+                <tr>
+                    <td>
+                        <label>Nom de l'entreprise</label>
+                    </td>
+                    <td>
+                        <input type='text' name='entreprise_trans' value =''>
+                    </td>
+                    <td>
+                        <label>Téléphone</label>
+                    </td>
+                    <td>
+                        <input type='text' name='tel_transp' value =''>
+                    </td>                  
+                </tr>
+                
+            </table>
+            <input type='submit'   name='bouton' value='valider'>
+            
 
         </form>
     </body>
