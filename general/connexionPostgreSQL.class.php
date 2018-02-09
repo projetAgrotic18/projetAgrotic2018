74a4fabbbcc812c734753f916ce1c5106b1dcfdc
Magicrevette <pierre.putcrabey@gmail.com>
2018-02-09 11:52:13 +0100

    explication postgreSQL

--- general/connexionPostgreSQL.class.php
+++ general/connexionPostgreSQL.class.php
@@ -1,35 +1,21 @@
 <?php
 
 class connexionPostgreSQL {
-    private $nom_bdd;
-    private $login;
-    private $mdp;
-    private $host;
-    private $port;
     private $connex;
     
     function __construct() {
-        $this->nom_bdd = testprojet;
-        $this->login = postgres;
-        $this->mdp = postgres;
-        $this->host = '194.199.251.139';
-        $this->port = '5433';
-        $this->connex = pg_connect("'host=".$this->host." port=".$this->port." dbname=".$this->nom_bdd." user =".$this->login." password=".$this->mdp."'")
+        $this->connex = pg_connect("host=194.199.251.139 port=5433 dbname=testprojet user=postgres password=postgres")
                 or die('Connexion impossible : ' . pg_last_error());
-        pg_set_client_encoding($this->connex, 'utf8mb4');
     }
     
     function fermer() {
-        // Libère le résultat
-        pg_free_result($this->result);
-
         // Ferme la connexion
         pg_close($this->connex);       
     }
     
     function requete($query) {
         $result = pg_query($this->connex, $query)
-            or die('Échec de la requête : ' . pg_last_error());
+            or die('�chec de la requ�te : ' . pg_last_error());
         return $result;
     }
 }
---
+++ general/test_bdd_postgre.php
@@ -0,0 +1,20 @@
+
+<?php
+
+require "../general/connexionPostgreSQL.class.php";
+$connex = new connexionPostgreSQL();
+$result = $connex->requete("SELECT * FROM communes");
+
+echo pg_num_rows($result)."</br>";
+echo pg_num_fields($result);
+echo "<table border=1 bordorcolor=black>";
+while ($row=pg_fetch_array($result,null,PGSQL_NUM)) {
+    echo "<tr>";
+    for($i=0; $i<pg_num_fields($result); $i++){
+        echo "<td>".$row[$i]."</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+
+$connex->fermer;
