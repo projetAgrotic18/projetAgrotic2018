9d02a06d5542f6a471d760d5cd14395f9d2865f4
Unknown <lea.cimetiere@agro-bordeaux.fr>
2018-02-13 15:48:52 +0100

    Page de connexion

---
+++ page d'accueil/Connexion.php
@@ -0,0 +1,11 @@
+    <center><h1>Exercices 8 PHP</h1></center><br>
+    <br><br>
+    
+    <h2>Formulaires et contrôles</h2>
+    <form method='GET' action='Exo8_part2.php'>
+        <p>Saisir votre nom</p><br/>
+        <input type='text' name='nom' size='10' value=''><br/>
+        <p>Saisir votre mot de passe</p><br/>
+        <input type='passeword' name='mdp' size='10'><br/><br/>
+        <input type='submit' name='bouton1' value='Soumettre'>
+    </form><br><br>
