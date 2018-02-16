2b634eab8a7244dfd9435bb59c25332643774e7b
Chloe <chloe.dupuis6@gmail.com>
2018-02-16 11:23:52 +0100

    modif diagnostic

--- diagnostic/diagnostic_v3.php
+++ diagnostic/diagnostic_v3.php
@@ -6,14 +6,25 @@
 
 	<!-- Section Javascript: définition de la fonction gérant la récupération des données -->
 	<script type="text/javascript">
+		function espece_animal(str){
+			$.ajax({
+				type: 'get',
+				url: 'diagnostic_v3_2.php',
+				data: {
+					porygon:str
+				},
+				success: function(response){
+					document.getElementById("symptomes").innerHTML=response;
+				}
+			});
+		}
 	</script>
 
 	</head>
 	<body>
 	<?php
-	//require "../general/connexionPostgreSQL.class.php";
-	//$connex = new connexionPostgreSQL();
-	//$result = $connex->requete("Mettre votre requête ici");
+	require "../general/connexionPostgreSQL.class.php";
+	$connex = new connexionPostgreSQL();	
 	?>
 	
 	<h1>Diagnostic vétérinaire</h1>
@@ -34,17 +45,21 @@
 	<input type="date" name="date" size="10"><br/><br/>
 	
 	<h2>Caractéristiques du diagnostic :</h2>
-	* Espèce : <br/>	
-	<input type=radio name="espece" value="bovin" onclick=>
-	<input type=radio name="espece" value="caprin" onclick=>
-	<input type=radio name="espece" value="ovin" onclick=>
-	<br/>
 	* Numéro d'identification : <br/>	
 	<input type="text" name="numero" size="20"><br/><br/>	
 	* Sexe : <br/>	
-	<input type=radio name="sexe" value="femelle" onclick=>
-	<input type=radio name="sexe" value="male" onclick=>
+	<input type=radio name="sexe" value="femelle" onclick=>Femelle
+	<input type=radio name="sexe" value="male" onclick=>Mâle
 	<br/>
+	* Espèce : <br/>	
+	<input type=radio name="espece" value="bovin" onclick=espece_animal(this.value)>Bovin
+	<input type=radio name="espece" value="caprin" onclick=espece_animal(this.value)>Caprin
+	<input type=radio name="espece" value="ovin" onclick=espece_animal(this.value)>Ovin
+	<br/>
+	
+	
+	
+	<span id="symptome"></id>
 
 	</form>
 	</body>
