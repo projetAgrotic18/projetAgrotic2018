f7b2267a1698090d4508b944724afd68092ba478
Chloe <chloe.dupuis6@gmail.com>
2018-02-16 15:00:17 +0100

    modif diagnostic

--- diagnostic/diagnostic_v3.php
+++ diagnostic/diagnostic_v3.php
@@ -22,14 +22,10 @@
 
 	</head>
 	<body>
-	<?php
-	require "../general/connexionPostgreSQL.class.php";
-	$connex = new connexionPostgreSQL();	
-	?>
+	<form method="GET" action="diagnostic_v3_2.php">
 	
 	<h1>Diagnostic vétérinaire</h1>
 	(*) : champs obligatoires <br/>	
-	<form method="GET" action="diagnostic_v3_2.php">
 	
 	<!--Caractéristiques-->
 	<h2>Caractéristiques généraux :</h2>
@@ -39,8 +35,8 @@
 	<input type="text" name="nom_exploitation" size="20"><br/>
 	* Numéro de l'exploitation : <br/>
 	<input type="text" name="numero_exploitation" size="20"><br/>
-	* Localisation : <br/>
-	<input type="text" name="localisation" size="20"><br/>
+	* Commune : <br/>
+	<input type="text" name="commune" size="20"><br/>
 	* Date : <br/>
 	<input type="date" name="date" size="10"><br/><br/>
 	
@@ -50,14 +46,59 @@
 	* Sexe : <br/>	
 	<input type=radio name="sexe" value="femelle" onclick=>Femelle
 	<input type=radio name="sexe" value="male" onclick=>Mâle
-	<br/>
+	<br/><br/>
 	* Espèce : <br/>	
 	<input type=radio name="espece" value="bovin" onclick=espece_animal(this.value)>Bovin
 	<input type=radio name="espece" value="caprin" onclick=espece_animal(this.value)>Caprin
 	<input type=radio name="espece" value="ovin" onclick=espece_animal(this.value)>Ovin
-	<br/>
-	<span id="symptome"></id>
+	<br/><br/>
+	<!--<span id="symptome"></id>-->
 
+	<?php
+	require "../general/connexionPostgreSQL.class.php";
+	$connex = new connexionPostgreSQL();	
+	
+	//Symptomes : 
+	echo "Symptomes : <br/>";	
+	$result = $connex->requete("SELECT symp.libelle_symptome FROM symp");
+	while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
+		echo "<input type=checkbox name='symptome' value=".$row[0].">".$row[0]."<br/>";
+	}
+	echo "<br/>";
+	//autre symptome
+	// echo "Autre symptome : <br/>";
+	// echo "<input type='text' name='autre_symptome' size='60' value=''><br/>";
+	// $result= $connex->requete("INSERT INTO symp(id_sympt, id_obl, libelle_symptome) VALUES('', '', '$autre_symptome')";
+	
+	//Maladies :
+	echo "Maladies : <br/>";
+	$result = $connex->requete("SELECT libelle_maladie FROM maladie");
+	while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
+		echo "<input type=checkbox name='maladie' value=".$row[0].">".$row[0]."<br/>";
+	}
+	echo "<br/>";
+	//autre maladie
+	// echo "Autre maladie : <br/>";
+	// echo "<input type='text' name='autre_maladie' size='60' value=''><br/>";
+	// $result= $connex->requete("INSERT INTO maladie(id_maladie, id_espece, libelle_maladie, cat_maladie, precautions) VALUES('', '', '$autre_maladie', '', '')";
+	
+	//Prélèvements :
+	echo "Prélèvements : <br/>";
+	$result = $connex->requete("SELECT libelle_prelevement FROM prelev");
+	while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
+		echo "<input type=checkbox name='prelevement' value=".$row[0].">".$row[0]."<br/>";
+	}
+	echo "<br/>";
+	
+	//Analyses :
+	echo "Analyses : <br/>";
+	$result2 = $connex->requete('SELECT libelle_analyse FROM "ANALYSE"');
+	while ($row = pg_fetch_array($result2, null, PGSQL_NUM)) {
+		echo "<input type=checkbox name='analyse' value=".$row[0].">".$row[0]."<br/>";
+	}
+	echo "<br/>";
+	?>
+	<INPUT type = "submit" value="Ajouter ce diagnostic">
 	</form>
 	</body>
 </html>
--- diagnostic/diagnostic_v3_2.php
+++ diagnostic/diagnostic_v3_2.php
@@ -8,34 +8,32 @@
 	Votre diagnostic a bien été ajouté. <br>
 	
 	<?php	
-	$espece=$_GET["porygon"];
+	// $espece=$_GET["porygon"];
 	
-	require "../general/connexionPostgreSQL.class.php";
-	$connex = new connexionPostgreSQL();	
-	
-	$result=$connex->requete("SELECT*FROM sympt");
-	
-	// $result = $connex->requete("SELECT sympt.libelle_symptome FROM sympt 
-				// JOIN symptdiag ON symptdiag.id_sympt=sympt.id_sympt 
+	// require "../general/connexionPostgreSQL.class.php";
+	// $connex = new connexionPostgreSQL();	
+		
+	// $result = $connex->requete("SELECT symp.libelle_symptome FROM symp 
+				// JOIN symptdiag ON symptdiag.id_sympt=symp.id_sympt
 				// JOIN diagnostic ON symptdiag.id_diagnostic=diagnostic.id_diagnostic 
 				// JOIN espece ON diagnostic.id_espece=espece.id_espece 
-				// WHERE espece.libelle_espece=".$_espece);
-	while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
-		echo $row[2];
-	}		
+				// WHERE espece.libelle_espece='.$_espece.'");
+	// while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
+		// echo $row[0];
+	// }	
+
 	
 	// if ($espece == 'bovin'){
 		// echo "<input type=checkbox name='symptome' value='avortement'>avortement <br/>";
 		// echo "<input type=checkbox name='symptome' value='avortement'>chute production laitière <br/>";
 		// echo "<input type=checkbox name='symptome' value='avortement'>conjonctivite <br/>";
+	// 
+	if (isset($_GET["nom_exploitant"] & isset($_GET["nom_exploitation"]) & isset($_GET["numero_exploitation"]) & isset($_GET["commune"]) & isset($_GET["date"]) & isset($_GET["numero"]) & isset($_GET["espece"])){
+		
+	}
+	
 	?>
 	
 	
-	<FORM action="diagnostic_v3.php">
-		<INPUT type = "submit" value="Consulter ce diagnostic">
-	</FORM>
-	<FORM action="diagnostic_v3.php">
-		<INPUT type = "submit" value="Ajouter un autre diagnostic">
-	</FORM>
 	</body>
 </html>
