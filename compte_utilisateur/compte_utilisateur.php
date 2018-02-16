f713a3c971485468c88ee8f491ca0c18872baddd
Unknown <thibaud.fournes@agro-bordeaux.fr>
2018-02-16 10:46:14 +0100

    vous pouvez maintenant créer un compte utilisateur :)

--- compte_utilisateur/compte_utilisateur.php
+++ compte_utilisateur/compte_utilisateur.php
@@ -9,16 +9,16 @@
 		
 		<!-- Radio-boutonpour sélectionner le type de formulaire à remplir-->
 		<form method = 'POST' name = 'form_eleveur' action = 'validation_compte_utilisateur.php'>	
-            <INPUT type = radio name = rb value = 'ddpp' >DDPP 
-            <INPUT type = radio name = rb value = 'gds'>GDS 
-            <INPUT type = radio name = rb value = 'veto'>Vétérinaire / GTV 
-            <INPUT type = radio name = rb value = 'labo'>Laboratoire 
+            <INPUT type = radio name = rb value = 'DDPP' >DDPP 
+            <INPUT type = radio name = rb value = 'GDS'>GDS 
+            <INPUT type = radio name = rb value = 'veterinaire'>Vétérinaire / GTV 
+            <INPUT type = radio name = rb value = 'laboratoire'>Laboratoire 
             <INPUT type = radio name = rb value = 'eleveur'>Eleveur <br/><br/><br/>
             Login : <input type=text name='login' placeholder='Login'><br/><br/>
-            Mot de passe : <input type=text name='mot_de_passe' placeholder='Mot de Passe'><br/><br/><br/>
+            Mot de passe : <input type=password name='mot_de_passe' placeholder='Mot de Passe'><br/><br/><br/>
             Nom : <input type=text name='nom' placeholder='Nom'><br/><br/><br/>
             Adresse : <input type=text name='adresse' placeholder='Adresse' size = 100><br/><br/>
-            Adresse (suite) : <input type=text name='adresse2' placeholder='Adresse (suite)'><br/><br/>
+            Adresse (suite) : <input type=text name='adresse2' placeholder='Adresse (suite)' size = 100><br/><br/>
             Commune : <input type=text name='commune' placeholder='Commune' size = 75> 
             Code postal : <input type=text name='code_postal' placeholder='Code Postal' size = 20><br/><br/> 
             Département : <input type=text name='departement' placeholder='Département' size = 100><br/><br/><br/> 
