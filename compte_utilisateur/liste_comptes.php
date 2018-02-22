c665c8cba7aa41af57a66388c133497467786c2c
Unknown <thibaud.fournes@agro-bordeaux.fr>
2018-02-22 14:19:55 +0100

    ajout popup confirmation suppression compte

--- compte_utilisateur/liste_comptes.php
+++ compte_utilisateur/liste_comptes.php
@@ -3,6 +3,17 @@
 		<META charset="UTF-8">
 	</head>
 	<body>
+	<script>
+		function confirm_s(id){
+			if(confirm("Voulez vous vraiment supprimer ce compte ?")){
+				window.location='valid_suppr.php?id_compte='+id
+			}
+			else{
+				alert("Le ompte utilisateur n'a pas été supprimé.")
+				window.location='liste_comptes.php'
+			}
+}
+	</script>
     <?php
     // Connexion, sélection de la base de données
         
@@ -12,7 +23,7 @@
 
     // Exécution de la requête SQL
         
-        $result = $connex->requete("SELECT cu.id_compte, cu.identifiant, tu.libelle_type_utilisateur FROM type_utilisateur tu JOIN compte_utilisateur cu ON tu.id_type_utilisateur=cu.id_type_utilisateur");
+        $result = $connex->requete("SELECT cu.id_compte, cu.identifiant, tu.libelle_type_utilisateur FROM type_utilisateur tu JOIN compte_utilisateur cu ON tu.id_type_utilisateur=cu.id_type_utilisateur GROUP BY tu.libelle_type_utilisateur, cu.id_compte ORDER BY tu.libelle_type_utilisateur, cu.identifiant");
         
     // Affichage des résultats en HTML
 
@@ -21,7 +32,7 @@
             $id = $row[0];
             $identifiant = $row[1];
             $type = $row[2];
-            echo "<td>".$identifiant."</td><td>".$type."</td><td><a href='valid_suppr.php?id_compte=".$id."'><img src='suppr.png' alt='supprimer'/></a></td></tr>";
+            echo "<td>".$identifiant."</td><td>".$type."</td><td><img src='suppr.png' alt='supprimer' onclick='confirm_s($id)'/></a></td></tr>";
         }
         echo "</tr></table>";
 
@@ -31,7 +42,9 @@
 
     // Ferme la connexion
 
-        pg_close($connex);
+        $connex->fermer();
+		
+		//<a href='valid_suppr.php?id_compte=".$id."'>
     ?>
 
     </body>
