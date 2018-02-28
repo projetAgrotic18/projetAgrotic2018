9e86fd0a6ce37c0c31d4346e9c8dc19d26f52b99
Unknown <meryl.boquillon@agro-bordeaux.fr>
2018-02-28 15:04:05 +0100

    annuaire final !

--- communication/annuaire.php
+++ communication/annuaire.php
@@ -6,7 +6,11 @@
          		
 		<!--- Style Sonnaille -->
         <link href="../general/front/style.css" rel="stylesheet">
-         
+        
+		<!--Deux lignes de code pour le tableau-->
+		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css"/>
+		<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+		
          <!-- Section Javascript : définition de la fonction gérant l'affichage de l'annuaire -->
 		<script type="text/javascript">
 			function annuaire(str){
@@ -19,23 +23,17 @@
 					},
 					success: function (response){
 						document.getElementById("listeAnnuaire").innerHTML=response;
+						$(document).ready(function() {
+						$('#example').DataTable();
+						});
 					}
 				});
 			}
-			function rechercheAnnuaire(str){
-				$.ajax({
-					type: 'get',
-					url: 'majListeAnnuaire.php',
-					dataType: "html",
-					data: { 
-						rechercheListe: str
-					},
-					success: function (response){
-						document.getElementById("listeAnnuaire").innerHTML=response;
-					}
-				});
-			}
-		</script> 
+			//Code pour la mise en forme du tableau (voir datatable)
+			$(document).ready(function() {
+				$('#example').DataTable();
+			});
+		</script>
     </head>
     <body>
     	<!-- Entête -->
@@ -79,17 +77,42 @@
 		
 					?>
     			</div>
-    			<div class="col-sm-8">
-					<BR/><BR/> 
-    				<FORM>
-    					<INPUT type="text" size="20" value="Recherche" onkeyup="rechercheAnnuaire(tihs.value)"></INPUT>
-    				</FORM>
-    			</div>
     		</div>
         	
 			<p>Annuaire :</p>
 			
-			<span id="listeAnnuaire"></span>
+			<span id="listeAnnuaire">
+				<?php
+				$result_all_compte =  $connex->requete("SELECT libelle_type_utilisateur AS Type, nom AS Nom, 
+									portable AS Telephone, mail AS Email FROM compte_utilisateur cu 
+									JOIN type_utilisateur tu ON cu.id_type_utilisateur=tu.id_type_utilisateur");
+
+				$nbr_col = pg_num_fields($result_all_compte);
+				?>
+				<TABLE border=1 id="example">
+					<THEAD>
+						<TR>
+							<?php
+							for($i = 0; $i < $nbr_col; $i++) {
+								$nom_champ = pg_field_name($result_all_compte, $i);
+								echo ("<TH>" . $nom_champ. "</TH>");
+							}
+							?>
+						</TR>
+					</THEAD>
+					<TBODY>
+						<?php
+						while ($row = pg_fetch_array($result_all_compte)){
+							echo "<TR>";
+							for ($j=0; $j < $nbr_col; $j++) {
+								echo "<td>".$row[$j]."</td>";
+							}
+							echo "</TR>";
+						}
+						?>
+					</TBODY>
+				</TABLE>
+			</span>
 			<BR/>
 			
 		</div>
