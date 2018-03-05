60c02eba9667b92eb25d58a6fbc7b499dea68f9c
Unknown <lea.cimetiere@agro-bordeaux.fr>
2018-03-05 15:22:44 +0100

    Maj pa

--- page d'accueil/Page_accueil.php
+++ page d'accueil/Page_accueil.php
@@ -1,4 +1,8 @@
-<?php session_start();?>
+<?php session_start();
+if (isset($_SESSION["id_compte"])==false){
+    header('Location: Connexion.php');
+    exit;
+}?>
 <html>
 <head>
     <META charset="UTF-8" />
