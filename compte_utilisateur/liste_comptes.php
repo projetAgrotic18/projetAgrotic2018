280b16c7a05df41acb1428b6f188640168f325a4
Unknown <lea.cimetiere@agro-bordeaux.fr>
2018-03-07 10:41:59 +0100

    maj

--- compte_utilisateur/liste_comptes.php
+++ compte_utilisateur/liste_comptes.php
@@ -1,28 +1,39 @@
 <html>
-	<head>
-		<META charset="UTF-8"/>
-        <link rel="stylesheet" href='../general/front/style.css'>    
-	</head>
-	<body>
-	<script>
-		function confirm_s(id){
-			if(confirm("Voulez vous vraiment supprimer ce compte ?")){
-				window.location='valid_suppr.php?id_compte='+id
-			}
-			else{
-				alert("Le ompte utilisateur n'a pas été supprimé.")
-				window.location='liste_comptes.php'
-			}
-}
-	</script>
-<<<<<<< HEAD
-=======
+<head>
+    <META charset="UTF-8"/>
+    <link rel="stylesheet" href='../general/front/style.css'> 
     
->>>>>>> test
-    <?php
-        include('../general/front/navigation.html');
-        echo "<center><h1>Comptes utilisateurs</h1></center><br><br>";
-        echo "<h2>Liste des comptes</h2><br><br>";
+    <!--Deux lignes de code pour le tableau-->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css"/>
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    
+    <script>
+        function confirm_s(id){
+            if(confirm("Voulez vous vraiment supprimer ce compte ?")){
+                window.location='valid_suppr.php?id_compte='+id
+            }
+            else{
+                alert("Le ompte utilisateur n'a pas été supprimé.")
+                window.location='liste_comptes.php'
+            }
+        }
+        //Code pour la mise en forme du tableau (voir datatable)
+        $(document).ready(function() {
+            $('#example').DataTable();
+        });
+    </script>
+</head>
+<body>
+    <!-- Entête -->
+    
+    <!-- Appelle de la page regroupant les fonctions -->
+        <?php require_once('../general/procedures.php'); ?>
+
+    <!-- Barre de navigation en fonction de l'utilisateur -->
+    <?php include('../general/switchbar.php'); 
+    
+    echo "<center><h1>Comptes utilisateurs</h1></center><br><br>";
+    echo "<h2>Liste des comptes</h2><br><br>";
         
     // Connexion, sélection de la base de données
         
@@ -34,17 +45,21 @@
         
         $result = $connex->requete("SELECT cu.id_compte, cu.identifiant, tu.libelle_type_utilisateur FROM type_utilisateur tu JOIN compte_utilisateur cu ON tu.id_type_utilisateur=cu.id_type_utilisateur GROUP BY tu.libelle_type_utilisateur, cu.id_compte ORDER BY tu.libelle_type_utilisateur, cu.identifiant");
         
-    // Affichage des résultats en HTML
+    // Affichage des résultats en HTML?>
+        <div class='padding'>
+            <table border=1 id="example"><tr><th>ID</th><th>Utilisateur</th><th>Type d'utilisateur</th><th></th></tr>
+                <?php while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
 
-        echo "<table><tr><th>Utilisateur</th><th>Type d'utilisateur</th><th>  </th></tr>";
-        while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
-            $id = $row[0];
-            $identifiant = $row[1];
-            $type = $row[2];
-            echo "<td>".$identifiant."</td><td>".$type."</td><td><img src='suppr.png' alt='supprimer' onclick='confirm_s($id)'/></a></td></tr>";
-        }
-        echo "</tr></table>";
+                    $id = $row[0];
+                    echo "<tr><td>".$row[0]."</td>";
+                    echo "<td>".$row[1]."</td>";
+                    echo "<td>".$row[2]."</td>";
+                    echo "<td><img src='suppr.png' alt='supprimer' onclick='confirm_s($id)'/></td></tr>";
+                }?>
+            </table>
+        </div>
 
+    <?php
     // Libère le résultat
 
         pg_free_result($result);
@@ -54,13 +69,8 @@
         $connex->fermer();
 		
 		//<a href='valid_suppr.php?id_compte=".$id."'>
-<<<<<<< HEAD
-    ?>
-=======
         echo "<br><br><br>";
         include('../general/front/footer.html');?>
-    
->>>>>>> test
 
     </body>
 </html>
--- page d'accueil/Page_accueil.php
+++ page d'accueil/Page_accueil.php
@@ -1,4 +1,4 @@
-<?php session_start();}?>
+<?php session_start();?>
 <html>
 <head>
     <META charset="UTF-8" />
