3f6016095c2e4ab32abc4adafc313cadbc85783f
Unknown <taoufik.manar@agrosupdijon.fr>
2018-03-07 10:45:08 +0100

    Auto stash before merge of "test" and "origin/test"

--- zone_tampon/liste_zone_tampon.php
+++ zone_tampon/liste_zone_tampon.php
@@ -1,4 +1,7 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
+<?php
+session_start(); 
+?>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
@@ -11,41 +14,48 @@ and open the template in the editor.
     </head>
     <body>
         
-           <form method="post" action="zone_tampon.php" name='creerzt'> 
+        <form method="post" action="zone_tampon_front.php" name='creerzt'> 
         <?php
-	require "../general/connexionPostgreSQL.class.php";
-	$connex = new connexionPostgreSQL();
-	$result = $connex->requete("SELECT libelle_maladie,rayon_prot,rayon_surv,id_zone_tampon,active FROM zone_tampon zt JOIN maladie m on m.id_maladie=zt.id_maladie where active='t'");
-	      
-	echo "<table border = 1 bordercolor = black>";
-	echo "<tr>";
+			require "../general/connexionPostgreSQL.class.php";
+			$connex = new connexionPostgreSQL();
 	
-		echo "<th>";
-		echo "Maladie";
-		echo "</th>";
-                echo "<th>";
-		echo "Rayon de protection";
-		echo "</th>";
-                echo "<th>";
-		echo "Rayon de Surveillance";
-		echo "</th>";
-	echo "</tr>";
+			//Récupération de l'id lors de la connexion (page d'accueil)
+			$id_type_utilisateur = $_SESSION["id_type_utilisateur"];
+			$result = $connex->requete("SELECT libelle_maladie,rayon_prot,rayon_surv,id_zone_tampon,active FROM zone_tampon zt JOIN maladie m on m.id_maladie=zt.id_maladie WHERE active='t'");
+	      
+		    if($id_type_utilisateur == 3 or $id_type_utilisateur ==6){
+				echo "<table border = 1 bordercolor = black>";
+				echo "<tr>";
+	
+				echo "<th>";
+				echo "Maladie";
+				echo "</th>";
+				echo "<th>";
+				echo "Rayon de protection";
+				echo "</th>";
+				echo "<th>";
+				echo "Rayon de Surveillance";
+				echo "</th>";
+				echo "</tr>";
             
        
         
-	while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
-		 echo "<tr>";
-		echo "<td>".$row[0]."</td>";
-                 echo "<td>".$row[1]."</td>";
-                 echo "<td>".$row[2]."</td>";
-                 echo "<td><a href='modif_zone_tampon.php?id_zone_tampon=".$row[3]."'>Modifier</a></td>";
-                   echo "<td><a href='desac_zone_tampon.php?id_zone_tampon=".$row[3]."'>D�sactiver la zone</a></td>";
-		echo "</tr>";
-	}
-        ?>
-        
-     
-            <input type="submit" value="Ajouter une zone tampon" name="Ajouter_zt" />
+				while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
+					echo "<tr>";
+					echo "<td>".$row[0]."</td>";
+					echo "<td>".$row[1]."</td>";
+					echo "<td>".$row[2]."</td>";
+					echo "<td><form action='modif_zone_tampon.php?id_zone_tampon=".$row[3]."' method='POST'><input type='submit' name='bt_submit_modif' value='Modifier'/></form></td>";
+					echo "<td><form action='desac_zone_tampon.php?id_zone_tampon=".$row[3]."' method='POST'><input type='submit' name='bt_submit' value='Désactiver la zone'/></form></td>";
+					echo "</tr>";
+				}
+				echo "<input type='submit' value='Ajouter une zone tampon' name='Ajouter_zt' />";
+			}
+			else{
+				echo "Vous n'avez pas les droits pour accéder à cette page";
+			}
+		?>
+			
         </form>
     </body>
 </html>
