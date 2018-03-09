7a3e56851333b3f5899c5ca25e0048e5731ac1f4
Chloe <chloe.dupuis6@gmail.com>
2018-03-09 17:12:29 +0100

    Auto stash before merge of "test" and "origin/test"

--- diagnostic/modif_diagnostic_validation.php
+++ diagnostic/modif_diagnostic_validation.php
@@ -1,8 +1,44 @@
 <?php session_start() ?>
 <html>
-	<head>
-	<META charset="UTF-8">
-	</head>
+<head>
+	<title>Confirmation de saisie d'un diagnostic</title>
+    <META charset="UTF-8"/>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+    <!--- Style Sonnaille -->
+    <link href="../general/front/style.css" rel="stylesheet">
+</head>
+<body>
+	<!-- Barre de navigation en fonction de l'utilisateur -->
+    <?php include('../general/switchbar.php'); ?>
+    
+    <div class="padding">   
+        <h1>Validation de maladie(s) associée(s) à un diagnostic </h1></br>
+        <p>Votre validation de maladie(s) a bien été prise en compte.</p>
+        <?php
+        require "../general/connexionPostgreSQL.class.php";
+        $connex = new connexionPostgreSQL();
+
+        $id_diagnostic=$_GET["id_diagnostic"];
+        $maladies=$_GET["maladies"];
+
+        //SUPPRESSION
+
+        $result= $connex->requete("DELETE FROM maladie_diag WHERE id_diagnostic = ".$id_diagnostic);
+
+        //INSERTION NOUVELLE(S) MALADIE(S) avec booleen en mode TRUE :
+        //maladies : $_GET["maladies"]
+        $maladies=$_GET["maladies"];
+        for ($i=0; $i<count($maladies); $i++){
+            $result= $connex->requete("INSERT INTO maladie_diag (id_maladie, id_diagnostic, confirme) VALUES ('".$maladies[$i]."','".$id_diagnostic."', TRUE)");
+        }
+
+        echo "</br>";
+        echo "<a href = 'consultation_diagnostic.php?id_diagnostic=$id_diagnostic'><button type='button'>Visualiser le diagnostic à nouveau</button></a></br></br>";
+        echo "<a href = 'liste_diagnostic.php'><button type='button'>Retourner à la liste de mes diagnostics</button></a>";
+        ?>
+    </div>
+	<?php include('../general/front/footer.html');?>
 	
 	<body>
 	
--- diagnostic/test_chloe.php
+++ diagnostic/test_chloe.php
@@ -7,27 +7,37 @@ $connex = new connexionPostgreSQL();
 	// On a bien l'id département du diagnostic 
 	//RECUPERATION DES ID COMPTE GDS DU MEME DEPARTEMENT 
 	
-	$result= $connex->requete("SELECT * FROM compte_utilisateur WHERE id_type_utilisateur=3");
-			
-	echo "<table border = 1 bordercolor = black>";
-
-	echo "<tr>";
-	for ($i=0 ; $i < pg_num_fields($result) ; $i++){
-		echo "<td>";
-		echo pg_field_name($result, $i);
-		echo "</td>";
-	}
-	echo "</tr>";
 	
-	while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
-		echo "<tr>";
-		for ($j=0 ; $j < pg_num_fields($result) ; $j++){
-			echo "<td>";
-			echo $row[$j];
-			echo "</td>";
+	$id_espece=array();
+	$result= $connex->requete("SELECT id_espece FROM espece");
+	for ($i=0; $i<count($result); $i++){
+		while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
+			$id_espece[$i]=$row[0];
 		}
-		echo "</tr>";
 	}
+	for ($i=0; $i<count($id_espece); $i++){
+		echo $id_espece[$i];
+	}
+			
+	// echo "<table border = 1 bordercolor = black>";
+
+	// echo "<tr>";
+	// for ($i=0 ; $i < pg_num_fields($result) ; $i++){
+		// echo "<td>";
+		// echo pg_field_name($result, $i);
+		// echo "</td>";
+	// }
+	// echo "</tr>";
+	
+	// while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
+		// echo "<tr>";
+		// for ($j=0 ; $j < pg_num_fields($result) ; $j++){
+			// echo "<td>";
+			// echo $row[$j];
+			// echo "</td>";
+		// }
+		// echo "</tr>";
+	// }
 
 ?>
 
