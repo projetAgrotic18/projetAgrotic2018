17460701ac779d4027c62189fe2266d47e852c12
Chloe <chloe.dupuis6@gmail.com>
2018-03-12 15:23:34 +0100

    Diagnostic - NOTIFS GDS QUI MARCHENT

--- diagnostic/modif_diagnostic_validation.php
+++ diagnostic/modif_diagnostic_validation.php
@@ -32,6 +32,61 @@
         for ($i=0; $i<count($maladies); $i++){
             $result= $connex->requete("INSERT INTO maladie_diag (id_maladie, id_diagnostic, confirme) VALUES ('".$maladies[$i]."','".$id_diagnostic."', TRUE)");
         }
+		
+		//RECUPERATION DEPARTEMENT POUR ENVOI AU GDS 
+	  $result= $connex->requete("SELECT id_commune FROM diagnostic WHERE id_diagnostic = ".$id_diagnostic);
+	  while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
+		$id_commune_diagnostic=$row[0];
+	  }
+  
+	  $result= $connex->requete("SELECT id_dpt FROM commune WHERE id_commune = ".$id_commune_diagnostic);
+	  while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
+		$id_dpt_diagnostic=$row[0];
+	  }
+	  //On a bien l'id du département du diagnostic. on va sélectionner les comptes GDS qui ont une commune dans le même département que le diagnostic.
+	  //RECUPERATION DES ID COMPTE GDS DU MEME DEPARTEMENT 
+	  $result= $connex->requete("SELECT co.id_compte 
+          FROM  commune c
+          JOIN compte_utilisateur co ON c.id_commune=co.id_commune 
+          WHERE co.id_type_utilisateur = 3 AND c.id_dpt=".$id_dpt_diagnostic);
+	
+	$id_comptes_GDS=array();
+	while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
+		for ($j=0 ; $j < pg_num_fields($result) ; $j++){
+			$id_comptes_GDS[]=$row[$j];
+		}
+	}	
+	  
+	//RECUPERATION DE l'ID NOTIFICATION :
+	$result = $connex->requete("SELECT max(id_notification) FROM notification");
+	while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
+		$id_notification = $row[0];
+	}
+
+	$id_notification = $id_notification +1;
+	$date = date("Y-m-d");
+	
+	//RECUPERATION DU OU DES LIBELLES MALADIE :
+	$maladies=$_GET["maladies"];
+	$libelle_maladie=array();
+	for ($i=0; $i<count($maladies); $i++){
+		$result= $connex->requete("SELECT libelle_maladie FROM maladie WHERE id_maladie=".$maladies[$i]);
+		while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
+			$libelle_maladie[]=$row[0];
+		}
+	}
+	//on a les maladies dans $libelle_maladie
+	
+	//ENVOI AUX MEMBRES DU GDS DU DEPARTEMENT CORRESPONDANT UNE NOTIF :
+    for ($i=0; $i<count($id_comptes_GDS); $i++){
+        $result= $connex->requete("INSERT INTO notification (id_notification, date_notification, titre_notification, message) 
+				VALUES ('".$id_notification."', '".$date."', 'Confirmation de maladie(s) dans votre département', 'Un cas de ".$libelle_maladie[0].$libelle_maladie[1].$libelle_maladie[2]." dans votre département a été recensé et validé par un vétérinaire.')");
+		$result= $connex->requete("INSERT INTO notification_compte (id_notification, id_compte, lu) 
+				VALUES ('".$id_notification."', '".$id_comptes_GDS[$i]."', FALSE)");
+		$id_notification = $id_notification +1;
+    }	  
+	  
+	//ENVOI A LA FRGDS
 
         echo "</br>";
         echo "<a href = 'consultation_diagnostic.php?id_diagnostic=$id_diagnostic'><button type='button'>Visualiser le diagnostic à nouveau</button></a></br></br>";
@@ -40,59 +95,5 @@
     </div>
 	<?php include('../general/front/footer.html');?>
 	
-	<body>
-	
-	<h1>Validation de maladie(s) associée à un diagnostic </h1>
-	Votre validation de maladie(s) a bien été prise en compte
-	<?php
-	require "../general/connexionPostgreSQL.class.php";
-	$connex = new connexionPostgreSQL();
-	
-	$id_diagnostic=$_GET["id_diagnostic"];
-	$maladies=$_GET["maladies"];
-	
-	//SUPPRESSION
-	
-	$result= $connex->requete("DELETE FROM maladie_diag WHERE id_diagnostic = ".$id_diagnostic);
-	
-	//INSERTION NOUVELLE(S) MALADIE(S) avec booleen en mode TRUE :
-	//maladies : $_GET["maladies"]
-	$maladies=$_GET["maladies"];
-	for ($i=0; $i<count($maladies); $i++){
-		$result= $connex->requete("INSERT INTO maladie_diag (id_maladie, id_diagnostic, confirme) VALUES ('".$maladies[$i]."','".$id_diagnostic."', TRUE)");
-	}
-	
-	//RECUPERATION DEPARTEMENT POUR ENVOI AU GDS 
-	$result= $connex->requete("SELECT id_commune FROM diagnostic WHERE id_diagnostic = ".$id_diagnostic);
-	while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
-		$id_commune_diagnostic=$row[0];
-	}
-	
-	$result= $connex->requete("SELECT id_dpt FROM commune WHERE id_commune = ".$id_commune_diagnostic);
-	while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
-		$id_dpt_diagnostic=$row[0];
-	}
-	// On a bien l'id du département du diagnostic. on va sélectionner les comptes GDS qui ont une commune dans le même département que le diagnostic.
-	//RECUPERATION DES ID COMPTE GDS DU MEME DEPARTEMENT 
-	$result= $connex->requete("SELECT co.id_compte 
-					FROM  commune c
-					JOIN compte_utilisateur co ON c.id_commune=co.id_commune 
-					WHERE co.id_type_utilisateur = 4 AND c.id_dpt=".$id_dpt_diagnostic);
-					
-	while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
-		$id_comptes_GDS=$row[0];
-	}
-	
-	//ENVOI AU GDS CORRESPONDANT D'UNE NOTIF
-	
-	//ENVOI A LA FRGDS
-	
-	echo "</br></br>";
-	echo "<a href = 'consultation_diagnostic.php?id_diagnostic=$id_diagnostic'><button type='button'>Visualiser le diagnostic à nouveau</button></a></br>";
-	echo "<a href = 'liste_diagnostic.php'><button type='button'>Retourner à la liste de mes diagnostics</button></a>";
-	?>
-	
-	
-	
-	</body>
+</body>
 </html>
--- diagnostic/test_chloe.php
+++ diagnostic/test_chloe.php
@@ -7,37 +7,42 @@ $connex = new connexionPostgreSQL();
 	// On a bien l'id département du diagnostic 
 	//RECUPERATION DES ID COMPTE GDS DU MEME DEPARTEMENT 
 	
-	
 	$id_espece=array();
-	$result= $connex->requete("SELECT id_espece FROM espece");
-	for ($i=0; $i<count($result); $i++){
-		while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
-			$id_espece[$i]=$row[0];
-		}
-	}
-	for ($i=0; $i<count($id_espece); $i++){
-		echo $id_espece[$i];
-	}
-			
-	// echo "<table border = 1 bordercolor = black>";
+	$result= $connex->requete("SELECT * FROM notification ");
 
-	// echo "<tr>";
-	// for ($i=0 ; $i < pg_num_fields($result) ; $i++){
-		// echo "<td>";
-		// echo pg_field_name($result, $i);
-		// echo "</td>";
-	// }
-	// echo "</tr>";
-	
 	// while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
 		// echo "<tr>";
 		// for ($j=0 ; $j < pg_num_fields($result) ; $j++){
 			// echo "<td>";
-			// echo $row[$j];
+			// $id_espece[]=$row[$j];
 			// echo "</td>";
 		// }
 		// echo "</tr>";
 	// }
+		
+	// echo $id_espece[0];
+	// echo "Deux". $id_espece[1];
+	// echo $id_espece[2];
+	
+	echo "<table border = 1 bordercolor = black>";
+
+	echo "<tr>";
+	for ($i=0 ; $i < pg_num_fields($result) ; $i++){
+		echo "<td>";
+		echo pg_field_name($result, $i);
+		echo "</td>";
+	}
+	echo "</tr>";
+	
+	while ($row = pg_fetch_array($result, null, PGSQL_NUM)) {
+		echo "<tr>";
+		for ($j=0 ; $j < pg_num_fields($result) ; $j++){
+			echo "<td>";
+			echo $row[$j];
+			echo "</td>";
+		}
+		echo "</tr>";
+	}
 
 ?>
 
